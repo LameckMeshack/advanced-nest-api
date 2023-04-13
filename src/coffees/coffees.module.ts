2df3c1da29737c3ccf9449adf6d1a093bf6848ef
Lameck Meshack <rhinolamer@gmail.com>
2023-04-13 13:19:20 +0300

    UseClass providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,12 +7,22 @@ import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './entities/coffee.constant';
 
+class ConfigService { }
+class DevelopmentConfigService { }
+class ProductionConfigService { }
 
+const configServiceProvider = {
+    provide: ConfigService,
+    useClass:
+        process.env.NODE_ENV === 'development'
+            ? DevelopmentConfigService
+            : ProductionConfigService,
+};
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
-    providers: [CoffeesService, { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
+    providers: [CoffeesService, configServiceProvider, { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
     // exports: [CoffeesService]
 
 })
