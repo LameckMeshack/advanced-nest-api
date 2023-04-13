6e8cc3b9bcd92b605d936bde29827dbd75029429
Lameck Meshack <rhinolamer@gmail.com>
2023-04-13 13:14:00 +0300

    Non class based providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,13 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './entities/coffee.constant';
 
-class MockCoffeeService { }
+
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
-    providers: [{ provide: CoffeesService, useValue: new MockCoffeeService() }],
+    providers: [CoffeesService, { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
+    // exports: [CoffeesService]
 
 })
-export class CoffeesModule { }
+export class CoffeesModule {
+}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
@@ -7,6 +7,8 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './entities/coffee.constant';
+
 
 
 @Injectable()
@@ -17,7 +19,10 @@ export class CoffeesService {
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>,
         private readonly connection: DataSource,
-    ) { }
+        @Inject(COFFEE_BRANDS) coffeeBrands: string[],
+    ) {
+        console.log(coffeeBrands[0]);
+    }
 
 
     findAll(paginationQuery: PaginationQueryDto) {
