0f349679dff135707222ed3d8df83deba5b839d2
Lameck Meshack <rhinolamer@gmail.com>
2023-04-14 15:37:22 +0300

    Custom decorator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -14,6 +14,7 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 // import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
 
 @Controller('coffees')
@@ -21,14 +22,17 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) { }
   @Get()
   @Public()
-  async findAll(@Query() paginationQuery: PaginationQueryDto) {
-    // await new Promise(resolve => setTimeout(resolve, 5000));
+  async findAll(
+    @Protocol() protocol: string,
+    @Query() paginationQuery: PaginationQueryDto) {
+      // await new Promise(resolve => setTimeout(resolve, 5000));
+      console.log(protocol, "protocol");
     return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    console.log(id)
+    // console.log(id)
     return this.coffeesService.findOne('' + id);
   }
 
